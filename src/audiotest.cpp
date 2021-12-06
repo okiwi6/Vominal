59b9588a0e82871626aa7a52550d0f09662755e2
okiwi64 <45100017+okiwi6@users.noreply.github.com>
2021-12-06 17:16:36 +0100

    make path absolute

--- src/audiotest.cpp
+++ src/audiotest.cpp
@@ -6,30 +6,39 @@
 #include "audio/SoundManager.hpp"
 #include <chrono>
 #include <thread>
+#include <stdint.h>
 
 
-int main() {
+int main(int argc, char* argv[]) {
     // sf::SoundBufferRecorder rec;
     // rec.start();
     // std::this_thread::sleep_for(std::chrono::milliseconds(3000));
     // rec.stop();
-    
+
+    std::string path = __FILE__;
+    size_t last_occ = path.find_last_of('/');
+    path = path.substr(0, last_occ) + "/../assets/test.ogg";
 
     // // std::this_thread::sleep_for(std::chrono::milliseconds(4000));
+    
     sf::SoundBuffer buffer; // = rec.getBuffer();
-    buffer.loadFromFile("../assets/test.ogg");
-
+    while(buffer.getSamples() == 0x0) {
+        buffer.loadFromFile(path);
+    }
+    
     // OutputStream out(buffer);
     // out.play();
 
 
     // while(out.getStatus() == OutputStream::Playing) {
-    //     sf::sleep(sf::seconds(0.1f));
+    sf::sleep(sf::seconds(0.1f));
     // }
 
     SoundManager sm;
     sm.addSound(buffer);
-    //sm.startRecording();
+    sf::sleep(sf::milliseconds(800));
+    sm.addSound(buffer);
+    sm.startRecording();
     
     while(true) {
         sm.update();
